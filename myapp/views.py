5db1e7e3dd1522ace685b162dd7247cbfd797254
Steveen <raizen707@gmail.com>
2026-02-07 07:26:11 -0600

    first push

---
+++ myapp/views.py
@@ -0,0 +1,28 @@
+import json
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+import time
+
+# Create your views here.
+def stream_json(request):
+    def generate():
+        for i in range(5):
+            data = {
+                "number": i,
+                "message": f"Hello {i}"
+            }
+            yield json.dumps(data) + "\n"
+
+    return StreamingHttpResponse(
+        generate(),
+        content_type="application/x-ndjson"
+    )
+
+
+def generate():
+    for i in range(5):
+        yield json.dumps({"step": i}) + "\n"
+        time.sleep(1)
+
+def stream_page(request):
+    return render(request, "stream.html")
